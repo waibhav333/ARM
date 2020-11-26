69fab032fff9eb6b64764988a70d076310a33016
waibhav333 <60283141+waibhav333@users.noreply.github.com>
2020-11-27 02:31:28 +0530

    Update gcd.s

--- gcd.s
+++ gcd.s
@@ -1,14 +1,15 @@
-     AREA     appcode2, CODE, READONLY
-     EXPORT __main
+     area     appcode, CODE, READONLY
+     export __main
 	 ENTRY 
-__main  FUNCTION		         
+__main  function		 
+       
          mov r0,#4
 	 mov r1,#10
-loop     cmp r0,r1	 
+loop cmp r0,r1	 
 	 subgt r0,r0,r1
 	 sublt r1,r1,r0
 	 bne loop
-stop	B stop; stop program	 
-		 
-     ENDFUNC
-     END
+	 
+stop b stop
+endfunc
+     end	
