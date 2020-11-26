65d96fb5575badc274682e60ef72b545cf1b54fd
waibhav333 <60283141+waibhav333@users.noreply.github.com>
2020-11-27 02:29:35 +0530

    Update fibo_series.s

--- fibo_series.s
+++ fibo_series.s
@@ -1,17 +1,16 @@
-     AREA     appcode, CODE, READONLY
-     EXPORT __main
+      area     appcode, CODE, READONLY
+     export __main
 	 ENTRY 
-__main  FUNCTION		         
+__main  function		 
+       
          mov r0,#0
 	 mov r1,#1
-	 mov r2,r0
 	 mov r2,r1
-loop     add r2,r0,r1
+loop add r2,r0,r1
 	 mov r0,r1
 	 mov r1,r2
 	 b loop
 	 
-stop	B stop; stop program	 
-		 
-     ENDFUNC
-     END
+stop b stop
+endfunc
+     end
